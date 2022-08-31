15a01a56e9a8076ab59a6ae0427a45fbc6ee1795
Gary Grubbs <gary.grubbs@gmail.com>
2022-08-31 17:37:15 -0500

    Clean up script tag

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,15 +7,13 @@ export default function App({ Component, pageProps }) {
   <>
     <Script key="ga-script" strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-5E8SMEDZT3" />
       <Script key="ga-script-local" strategy='afterInteractive'>
-        {
-          `
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
           gtag('config', 'G-5E8SMEDZT3');
-          `
-        }
+        `}
       </Script>
     <Component {...pageProps} />
   </>
