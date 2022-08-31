d0472a36d32465990f44631870e51ed745fa0ed2
Gary Grubbs <gary.grubbs@gmail.com>
2022-08-31 15:34:47 -0500

    Moved GA to _app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,23 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Script key="ga-script" strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} />
+      <Script key="ga-script-local" strategy='afterInteractive'>
+        {
+          `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', ${process.env.GOOGLE_ANALYTICS_ID});
+          `
+        }
+      </Script>
+    <Component {...pageProps} />
+  </>
+  )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,6 @@ import { Benefits } from '@/components/Benefits'
 import { Hero } from '@/components/Hero'
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
 import { SecondaryFeatures } from '@/components/SecondaryFeatures'
-import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -15,19 +14,7 @@ export default function Home() {
         <meta
           name="description"
           content="dbExpression closes the gap between application code and native SQL, bringing Microsoft SQL Server functionality into .NET."
-        />
-        <Script key="ga-script" strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} />
-        <Script key="ga-script-local" strategy='afterInteractive'>
-          {
-            `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', ${process.env.GOOGLE_ANALYTICS_ID});
-            `
-          }
-        </Script>
+      />
       </Head>
       <Header />
       <main>
