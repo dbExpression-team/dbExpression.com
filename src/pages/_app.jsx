0145b8192d2304f71e796459a43e189eeb93873a
Gary Grubbs <gary.grubbs@gmail.com>
2022-10-26 08:44:35 -0500

    moved ga-tag id to env file

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,14 +5,14 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return (
   <>
-    <Script key="ga-script" strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-5E8SMEDZT3" />
-      <Script key="ga-script-local" strategy='afterInteractive'>
+    <Script key="ga-script" strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`} />
+      <Script id="ga-script-local" strategy='afterInteractive'>
         {`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
         
-          gtag('config', 'G-5E8SMEDZT3');
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
         `}
       </Script>
     <Component {...pageProps} />
