1c7b372d01e1597579216ffc61b080e54238cfc4
gwgrubbs <gary.grubbs@gmail.com>
2024-03-26 14:32:38 -0500

    Changed namespace references to DbExpression, use local images for social.

--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -71,7 +71,7 @@ const features = [
   },
   {
     id: 'NuGet',
-    url: 'https://www.nuget.org/packages/HatTrick.DbEx.MsSql',
+    url: 'https://www.nuget.org/packages/DbExpression.MsSql',
     summary: 'Available from NuGet',
     description: 'dbExpression is quick and easy to get up and running using a couple of packages and a dotnet tool - all available on NuGet.',
     icon: function NuGetIcon() {
